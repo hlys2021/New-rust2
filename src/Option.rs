ade24bc0991b293c720e34e468098c77026c6f92
hlys2021 <84297799+hlys2021@users.noreply.github.com>
2023-03-22 18:03:16 +0800

    三月第四课
    06作业

---
+++ src/Option.rs
@@ -0,0 +1,20 @@
+fn sum_of_numbers(numbers: &[u32]) -> Option<u32> {
+    let mut sum = 0;
+
+    for &number in numbers {
+        match sum.checked_add(number) {
+            Some(new_sum) => sum = new_sum,
+            None => return None, // 溢出时返回None
+        }
+    }
+
+    Some(sum)
+}
+
+fn main() {
+    let numbers = &[1, 2, 3, 4, 5];
+    match sum_of_numbers(numbers) {
+        Some(sum) => println!("集合的和: {}", sum),
+        None => println!("溢出"),
+    }
+}
